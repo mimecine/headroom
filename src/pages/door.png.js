e6ea5340e09f131acb8d3bb3a3339e69ac974104
Marco Romeny <marco@mimecom.net>
2025-02-08 16:54:13 +0000

    Tokens for blobs

--- src/pages/door.png.js
+++ src/pages/door.png.js
@@ -1,17 +1,14 @@
 import fs from 'node:fs/promises';
 import { getStore } from '@netlify/blobs';
-const store = getStore({ name: 'door', consistency: 'strong' });
 const prerender = false;
 
-// export async function GET({ params, request }) {
-//     const open = new Date(await store.get('open')).getTime() > Date.now() + 1000 * 1000;
-//     console.log(open, new Date(await store.get('open')).getTime() % 10000, (Date.now() + 1000 * 10) % 10000);
-
-//     const file = await fs.readFile(`./src/assets/images/${open ? 'open' : 'closed'}.png`);
-//     return new Response(file);
-// }
-
 export async function GET({ params, request }) {
+    const store = getStore({
+        name: 'door',
+        consistency: 'strong',
+        siteID: '4100fd91-6e10-4220-9ec9-152893b5ef95',
+        token: 'nfp_nYnWkqa38c17E6s6od2yos4q3edukjek298e'
+    });
     const expireTime = new Date(await store.get('open')).getTime();
     const isOpen = expireTime > Date.now();
     const file = await fs.readFile(`./src/assets/images/${isOpen ? 'open' : 'closed'}.png`);
