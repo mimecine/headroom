5ed0d1f2d4ab019049c52eebdb03245702624c4d
Marco Romeny <marco@mimecom.net>
2025-02-10 14:09:35 +0000

    Ghhh

--- src/pages/door.png.js
+++ src/pages/door.png.js
@@ -7,11 +7,9 @@ export async function GET({ params, request, redirect }) {
         name: 'door',
         consistency: 'strong'
     });
-    const expireTime = new Date(await store.get('open')).getTime();
-    const isOpen = expireTime > Date.now();
-    const file = await fs.readFile(`./src/assets/images/${isOpen ? 'open' : 'closed'}.png`);
-    // return Response.redirect(`/images/${isOpen ? 'open' : 'closed'}.png`);
-    console.log('open', isOpen);
-    console.log('expireTime', expireTime, `/images/${isOpen ? 'open' : 'closed'}.png`);
-    return redirect(`/images/${isOpen ? 'open' : 'closed'}.png`);
+    let expireTime = new Date(await store.get('open')).getTime();
+    let isOpen = expireTime > Date.now();
+    let file = await fs.readFile(`./src/assets/images/${isOpen ? 'open' : 'closed'}.png`);
+    return new Response(file, { headers: { 'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=0, must-revalidate' } });
+    // return new Response('Hello');
 }
