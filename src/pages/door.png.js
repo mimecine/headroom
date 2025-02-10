e010f844de03b7810e63fbbc32c99283debdd0a7
Marco Romeny <marco@mimecom.net>
2025-02-10 12:15:55 +0000

    return the Response.redirect

--- src/pages/door.png.js
+++ src/pages/door.png.js
@@ -10,6 +10,6 @@ export async function GET({ params, request }) {
     const expireTime = new Date(await store.get('open')).getTime();
     const isOpen = expireTime > Date.now();
     const file = await fs.readFile(`./src/assets/images/${isOpen ? 'open' : 'closed'}.png`);
-    Response.redirect(`/images/${isOpen ? 'open' : 'closed'}.png`);
+    return Response.redirect(`/images/${isOpen ? 'open' : 'closed'}.png`);
     //    return new Response(file);
 }
